3422fb28555feb72ff9f43b611b325a259643a78
jaweria332 <hiyamughal03@gmail.com>
2020-10-26 12:35:47 -0700

    Till Basic shapes to images

--- BasicConcepts.py
+++ BasicConcepts.py
@@ -35,7 +35,7 @@ while True:
     cv2.imshow("Video", img)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-"""
+
 
 #-----------------------BASIC FUNCTIONS LIKE GRAY, BLUR, EDGE DETECTOR, DIALATION, ERROTION----------------------------#
 
@@ -47,12 +47,37 @@ kernel=np.ones((3,3) , np.uint8)
 imgDial = cv2.dilate(imgEdge, kernel, iterations=1)
 
 imgEroder = cv2.erode(imgDial, kernel, iterations=1)
+
+imgResize = cv2.resize(img, (300, 300))
+imgCropped = img[0:200, 200:500]
+
 #Displaying image
 cv2.imshow("Lena", img)
 cv2.imshow("Gray", gray)
 cv2.imshow("Blur", blurImg)
 cv2.imshow("Edged", imgEdge)
 cv2.imshow("Eroded", imgEroder)
+cv2.imshow("Resized", imgResize)
+cv2.imshow("Cropped", imgCropped)
+"""
 
+#-------------------------------------------------SHAPES--------------------------------------------------------#
+img = np.zeros((512, 512, 3), np.uint8)
+print(img.shape)
+
+#Drawing line
+cv2.line(img, (0,0), (250, 250), (0,0,255), 3)
+cv2.line(img, (250,250), (512, 0), (0,0,255), 3)
+cv2.line(img, (0,0), (512, 0), (0,0,255), 3)
+
+#Drawing Rectangle
+cv2.rectangle(img, (200, 200),(300, 300), (255, 0, 255), 5)
+
+#Drawing circle on images
+cv2.circle(img, (251, 253), 30, (255, 255, 0), 5)
+
+#Put text
+cv2.putText(img, "OPEN HERE", (151,150), cv2.FONT_HERSHEY_COMPLEX,1, (0,150,150), 1)
+cv2.imshow("Image",img)
 #Wait indefinitely
 cv2.waitKey(0)
