cf52ec95605366f6b388701dd75b3958cb1e4f84
jaweria332 <hiyamughal03@gmail.com>
2020-10-27 03:32:12 -0700

    Color Detection updated

--- BasicConcepts.py
+++ BasicConcepts.py
@@ -112,22 +112,23 @@ cv2.createTrackbar("Sat Max", "Trackbar", 255, 255, empty)
 cv2.createTrackbar("Val Min", "Trackbar", 0, 255, empty)
 cv2.createTrackbar("Val Max", "Trackbar", 255, 255, empty)
 
+path = "car.jpg"
+
 while True:
-    img = cv2.imread("car.jpg")
+    img = cv2.imread(path)
     img = cv2.resize(img, (400, 300))
-    imgHSV=cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
     h_min = cv2.getTrackbarPos("Hue Min", "Trackbar")
     h_max = cv2.getTrackbarPos("Hue Max", "Trackbar")
     s_min = cv2.getTrackbarPos("Sat Min", "Trackbar")
     s_max = cv2.getTrackbarPos("Sat Max", "Trackbar")
     v_min = cv2.getTrackbarPos("Val Min", "Trackbar")
     v_max = cv2.getTrackbarPos("Val Max", "Trackbar")
-    print(h_max, h_min, s_max, s_min, v_max, v_min)
+    print(h_min, h_max,s_min,s_max,v_min,v_max)
     lower = np.array([h_min, s_min,v_min])
     upper = np.array([h_max, s_max, v_max])
     mask = cv2.inRange(imgHSV, lower, upper)
     imgRes = cv2.bitwise_and(img, img, mask=mask)
-
     cv2.imshow("Original", img)
     cv2.imshow("HSV", imgHSV)
-    cv2.waitKey(0)
+    cv2.waitKey(1)
