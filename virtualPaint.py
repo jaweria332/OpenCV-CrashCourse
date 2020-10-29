19d01de72624b38be0aa217605d1cfda0ab12ef4
jaweria332 <hiyamughal03@gmail.com>
2020-10-29 08:49:11 -0700

    updated file

--- virtualPaint.py
+++ virtualPaint.py
@@ -8,16 +8,16 @@ cap.set(3,frameWidth)
 cap.set(4, frameHeight)
 cap.set(10, 150)
 
-mycolor = []
+mycolor = [[0, 128, 188,255,0,255]]
 
 def findColor(img):
     img = cv2.resize(img, (400, 300))
     imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-    lower = np.array([h_min, s_min, v_min])
-    upper = np.array([h_max, s_max, v_max])
-    # Mask give us filtered out imae of that color
+    lower = np.array(mycolor[0][0:3])
+    upper = np.array(mycolor[0][3:6])
+    # Mask give us filtered out image of that color
     mask = cv2.inRange(imgHSV, lower, upper)
-    cv2.imshow("image", mask)
+    cv2.imshow(str(mycolor), mask)
 
 while True:
     success, img = cap.read()
