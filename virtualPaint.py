45e794715a40ebc7cc0ff6934b677479b13d251e
jaweria332 <hiyamughal03@gmail.com>
2020-10-29 09:18:48 -0700

    Updated files

--- virtualPaint.py
+++ virtualPaint.py
@@ -37,17 +37,20 @@ def findColor(img,myColors,myColorValues):
 
 
 
-def getContour(img):
-    contour, hierarchy = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-    x, y, w, h = 0,0,0,0
-    for cnt in contour:
+def getContours(img):
+    contours,hierarchy = cv2.findContours(img,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
+    x,y,w,h = 0,0,0,0
+    for cnt in contours:
         area = cv2.contourArea(cnt)
-        if area> 500:
-            cv2.drawContours(imgResult, cnt, -1, (255, 0, 0), 3)
-            peri = cv2.arcLength(cnt, True)
-            approx = cv2.approxPolyDP(cnt, 0.02 * peri, True)
+        if area>500:
+            #cv2.drawContours(imgResult, cnt, -1, (255, 0, 0), 3)
+            peri = cv2.arcLength(cnt,True)
+            approx = cv2.approxPolyDP(cnt,0.02*peri,True)
             x, y, w, h = cv2.boundingRect(approx)
-    return x+w//2, y
+    return x+w//2,y
+
+
+
 def drawCanvas(mypoints, myclrval, )
 while True:
     success, img = cap.read()
